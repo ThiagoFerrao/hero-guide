b5b26195b8fb87a898c02da79bae20a787e24a63
Thiago Cruz <thiago.ferrao91@gmail.com>
2018-07-08 14:23:01 -0300

    creates the project

---
+++ Hero Guide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hero Guide
+//
+//  Created by Thiago Ferrão on 08/07/18.
+//  Copyright © 2018 Thiago Ferrão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
