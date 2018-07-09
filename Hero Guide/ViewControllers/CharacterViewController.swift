671b38fd67745b36579aead3e62c2d9c7ff8ab7d
Thiago Cruz <thiago.ferrao91@gmail.com>
2018-07-08 21:26:09 -0300

    creates the character view controller

---
+++ Hero Guide/ViewControllers/CharacterViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CharacterViewController.swift
+//  Hero Guide
+//
+//  Created by Thiago Ferrão on 08/07/18.
+//  Copyright © 2018 Thiago Ferrão. All rights reserved.
+//
+
+import UIKit
+
+class CharacterViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
